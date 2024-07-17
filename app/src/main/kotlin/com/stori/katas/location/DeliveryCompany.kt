ccaf3b395e4c4c877418567cd7dc54e3c4ca5a12
Antonio (luis.dominguez@storicard.com) <luis.dominguez@storicard.com>
2024-07-17 12:35:11 -0600

    Init

--- app/src/main/kotlin/com/stori/katas/location/DeliveryCompany.kt
+++ app/src/main/kotlin/com/stori/katas/location/DeliveryCompany.kt
@@ -1,18 +1,7 @@
 package com.stori.katas.location
 
-private const val ZIPCODE_LENGTH = 5
-class DeliveryCompany (
-    private val location: ILocation
-) {
+class DeliveryCompany () {
     fun execute(zipCode: String ): String {
-        if (zipCode.length != ZIPCODE_LENGTH)
-            return "INVALID LENGTH"
-
-        val state = location.getStateByZipcode(zipCode)
-        return if (state == "CDMX") {
-            "DHL"
-        } else {
-            "RED PACK"
-        }
+        return TODO()
     }
 }
--- app/src/test/kotlin/com/stori/katas/location/LocationTest.kt
+++ app/src/test/kotlin/com/stori/katas/location/LocationTest.kt
@@ -22,37 +22,10 @@ class LocationTest {
 
     @Test
     fun `delivery company with zipcode length different to 5 returns error`() {
-        val zipCode = "" // Arrange
+        // Arrange
 
-        val result = SUT.execute(zipCode) // Act
+        // Act
 
-        assertEquals("INVALID LENGTH", result) // Assert
-    }
-
-    @Test
-    fun `delivery company with zipcode from CDMX returns DHL`() {
-        val cdmxZipCode = "11111" // Arrange
-
-        val result = SUT.execute(cdmxZipCode) // Act
-
-        assertEquals("DHL", result) // Assert
-    }
-
-    @Test
-    fun `delivery company with zipcode from JALISCO returns RED PACK`() {
-        val jaliscoZipCode = "22222" // Arrange
-
-        val result = SUT.execute(jaliscoZipCode) // Act
-
-        assertEquals("RED PACK", result) // Assert
-    }
-
-    @Test
-    fun `delivery company with zipcode from COLIMA returns NO DELIVERY`() {
-        val colimaZipCode = "55555" // Arrange
-
-        val result = SUT.execute(colimaZipCode) // Act
-
-        assertEquals("NO DELIVERY", result) // Assert
+        // Assert
     }
 }
