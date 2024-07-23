b4b0498151102594a5c40cf5f3585d0839bbc612
Luis Miguel Cabral Guzman <luis.cabral@storicard.com>
2024-07-23 13:26:09 -0600

    1 Fail test

--- app/src/test/kotlin/com/stori/katas/fizzBuzz/model/CalculateFizzBuzzMustReturn.kt
+++ app/src/test/kotlin/com/stori/katas/fizzBuzz/model/CalculateFizzBuzzMustReturn.kt
@@ -12,5 +12,12 @@ class CalculateFizzBuzzMustReturn {
         "2" isEqualTo result
     }
 
+    @Test
+    fun `success if the number is fizz`() = runTest {
+        val result = CalculateFizzBuzz().execute(3)
+
+        "Fizz" isEqualTo result
+    }
+
     private infix fun Any.isEqualTo(other: Any) = assertEquals(this, other)
 }
